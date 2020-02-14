f62434d4c7a95f989e68db5af5f5bb49107c26ea
Audrow <audrow.nash@gmail.com>
2020-02-14 08:46:59 -0800

    Make launch for qt_robot_pi + update to use rosparams

--- qt_robot_pi/scripts/start_face_server.py
+++ qt_robot_pi/scripts/start_face_server.py
@@ -1,21 +1,32 @@
-#!/usr/bin/env python                                                           
+#!/usr/bin/env python
+
 import rospy
 import subprocess
 from std_srvs.srv import Trigger, TriggerResponse
 import os
 
-def callback_srv(data):
-    os.system("pkill luakit")
-    process = subprocess.Popen("luakit --display=:0 -U http://192.168.100.2:8081/KiwiLite.html", shell=True)
-    # process = subprocess.Popen("echo hi", shell=True)
+
+kill_command = "pkill luakit"
+start_command = "luakit --display=:0 -U {url}".format(
+    url=rospy.get_param('qt_robot/face/url')
+)
+
+
+def callback_srv(_):
+    os.system(kill_command)
+    process = subprocess.Popen(start_command, shell=True)
 
     resp = TriggerResponse()
     resp.success = True
     resp.message = "Browser started with PID: %d" % process.pid
     return resp
 
+
 if __name__ == "__main__":
+
     rospy.init_node("pi_browser_starter_server")
-    srv = rospy.Service('start_pi_browser', Trigger, callback_srv)
-    print("ready")
+
+    service_name = rospy.get_param("qt_robot/face/start_browser_on_pi_service")
+    srv = rospy.Service(service_name, Trigger, callback_srv)
+    rospy.loginfo("Ready to start the face web browser")
     rospy.spin()
