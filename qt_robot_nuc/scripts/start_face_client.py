e5adc2cd17f369eb7f0e9ab3abbc876e09910703
Audrow <audrow.nash@gmail.com>
2020-02-14 08:46:39 -0800

    Make launch for qt_robot_nuc + update to use rosparams

--- qt_robot_nuc/scripts/start_face_client.py
+++ qt_robot_nuc/scripts/start_face_client.py
@@ -5,10 +5,11 @@ from std_srvs.srv import Trigger
 
 if __name__ == "__main__":
 	rospy.sleep(3)
-	rospy.wait_for_service('start_pi_browser')
-	service_call = rospy.ServiceProxy('start_pi_browser', Trigger)
+	service_name = rospy.get_param('qt_robot/display/start_browser_on_pi_service')
+	rospy.wait_for_service(service_name)
+	service_call = rospy.ServiceProxy(service_name, Trigger)
 	try:
 		response = service_call()
 		print(response)
 	except rospy.ServiceException, e:
-		print ("Service call failed: %s" % e)
+		print("Service call failed: %s" % e)
